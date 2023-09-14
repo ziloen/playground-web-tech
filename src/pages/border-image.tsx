efae7437d1a6ed7fc7d809142f2f2eb5cd6b70c1
N <34268416+ziloen@users.noreply.github.com>
2023-09-14 10:57:13 +0800

    chore: init

---
+++ src/pages/border-image.tsx
@@ -0,0 +1,9 @@
+
+
+
+export default function BorderImage() {
+
+  return (
+    <div></div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,58 +1,8 @@
-import { Counter } from '~/components/Counter'
-import { useI18n } from '~/hooks'
-
-
 export default function Index() {
-  const name = useRef<HTMLInputElement>(null)
-  const { t } = useI18n()
-
-  const navigate = useNavigate()
-  function go() {
-    if (name.current)
-      navigate(`/hi/${encodeURIComponent(name.current.value)}`)
-  }
 
   return (
     <div>
-      <div className="i-carbon-campsite text-4xl inline-block" />
-      <p>
-        <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">
-          Vitesse Lite
-        </a>
-      </p>
-      <p>
-        <em className="text-sm op75">Opinionated Vite Starter Template</em>
-      </p>
-
-      <div className="py-4" />
-
-      <Counter />
-
-      <input
-        ref={name}
-        id="input"
-        placeholder={t('placeholder')}
-        type="text"
-        className="px-4 py-2 w-250px text-center bg-transparent outline-none outline-active:none border border-rounded border-gray-200 border-dark:gray-700"
-        onKeyDown={({ key }) => key === 'Enter' && go()}
-      />
-
-      <div>
-        <button
-          className="m-3 text-sm btn"
-          disabled={!name}
-          onClick={go}
-        >
-          Go
-        </button>
-      </div>
-
-      <div>
-        {t('useI18nTest', {
-          link: <a className='text-blue' />,
-          name: <span className='text-green'>Dynamic Content</span>
-        })}
-      </div>
+      Web Tech
     </div>
   )
 }
