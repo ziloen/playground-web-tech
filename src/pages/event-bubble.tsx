12fdff799525664dc9dcf654c0dc3a9216ff45a9
N <34268416+ziloen@users.noreply.github.com>
2023-09-20 16:01:42 +0800

    chore: add event page

---
+++ src/pages/event-bubble.tsx
@@ -0,0 +1,8 @@
+
+
+
+export function EventBubble() {
+
+
+  return null
+}
---
+++ src/pages/event-loop.tsx
@@ -0,0 +1,17 @@
+
+
+export default function EventLoop() {
+  // MacroTask
+  // MicroTask
+  // AnimationFrame
+  // requestIdleCallback
+  // requestAnimationFrame
+  // setTimeout
+  // Promise
+  // MutationObserver
+  // IntersectionObserver
+  // SetInterval
+  // Call Stack
+
+  return null
+}
