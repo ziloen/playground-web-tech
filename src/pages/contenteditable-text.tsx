c2993f530146f10ce1eec5a6c0ba9dec2c244cf6
N <34268416+ziloen@users.noreply.github.com>
2023-09-26 12:13:01 +0800

    refact: remove outdate code

--- src/pages/contenteditable-text.tsx
+++ src/pages/contenteditable-text.tsx
@@ -5,68 +5,79 @@ import { useEventListener } from '~/hooks'
 export default function ContentEditableText() {
   const ref = useRef<HTMLDivElement>(null!)
 
-  useEventListener('beforeinput', e => {
-    const { inputType, dataTransfer, data } = e
-
-    // 取消所有 fomat 相关的 input
-    if (inputType.startsWith('format')) return e.preventDefault()
-
-    console.log('beforeinput', e)
-
-    if (data || !dataTransfer) return
-
-    if (inputType === 'insertFromPaste' || inputType === 'insertFromDrop') {
-      const html = dataTransfer.getData('text/html')
-      const text = dataTransfer.getData('text/plain')
-      if (!text) return e.preventDefault()
-
-      if (html) {
-        e.preventDefault()
-
-        // insertNode 不能使用 undo redo，只有 execCommand 可以
-        // const targetRange = e.getTargetRanges()[0]
-        // const newRange = document.createRange()
-        // newRange.setStart(targetRange.startContainer, targetRange.startOffset)
-        // newRange.setEnd(targetRange.endContainer, targetRange.endOffset)
-        // newRange.deleteContents()
-        // newRange.insertNode(document.createTextNode(text))
-
-        document.execCommand('insertText', false, text)
-      }
-    }
-  }, { target: ref })
+  useEventListener('beforeinput', onBeforeInput, { target: ref })
 
   useEventListener('input', (e: InputEvent) => {
     const inputType = e.inputType as InputType
-    // console.log('onInput', inputType)
   }, { target: ref })
 
   return (
-    <div className='grid h-full w-full grid-rows-[auto_1fr_auto]'>
+    <div
+      className='grid h-full w-full'
+      style={{
+        gridTemplateRows: 'auto minmax(0, 1fr) max-content',
+        gridTemplateColumns: '1fr',
+      }}
+    >
       {/* add backdrop-filter blur */}
       <div className='border-b '>
-        HAHSSHH
+        Chat room
       </div>
 
       <div className=''>
-
+        chat content
       </div>
 
-      {/* [ ] 1. drag to resize the height */}
-      {/* [ ] 2. paste and drop html to plaintext */}
-      {/* [x] 3. format input like Ctrl + B / Ctrl + I */}
-      {/* [ ] 4. max length */}
-      {/* [ ] 5. focus({ cursor: "start" | "end" | "all" | undefined }) */}
+      {/* [ ] drag to resize the height */}
+      {/* [x] paste and drop html to plaintext */}
+      {/* [x] format input like Ctrl + B / Ctrl + I */}
+      {/* [ ] max length */}
+      {/* [ ] focus({ cursor: "start" | "end" | "all" | undefined }) */}
+      {/* [ ] overflow issue when input */}
       <div
         ref={ref}
         contentEditable
         suppressContentEditableWarning
+        autoFocus
         tabIndex={0}
         style={{ wordBreak: 'break-word' }}
-        className='min-h-34px whitespace-pre-wrap h-max overflow-y-auto leading-17px border-t text-14px outline-none max-w-full'>
+        className='min-h-34px whitespace-pre-wrap overflow-y-auto box-border leading-17px border-t text-14px outline-none max-w-full'>
       </div>
     </div>
   )
+}
+
+
+/** remove style before input */
+function onBeforeInput(e: InputEvent) {
+  const { inputType, dataTransfer, data } = e
+
+  // prevent format input
+  if (inputType.startsWith('format')) return e.preventDefault()
+
+  console.log('beforeinput', e)
+
+  if (data || !dataTransfer) return
+
+  if (inputType === 'insertFromPaste' || inputType === 'insertFromDrop') {
+    const html = dataTransfer.getData('text/html')
+    const text = dataTransfer.getData('text/plain')
+    if (!text) return e.preventDefault()
+
+    if (html) {
+      e.preventDefault()
+
+      // insertNode can not undo / redo，only execCommand can
+      // const targetRange = e.getTargetRanges()[0]
+      // const newRange = document.createRange()
+      // newRange.setStart(targetRange.startContainer, targetRange.startOffset)
+      // newRange.setEnd(targetRange.endContainer, targetRange.endOffset)
+      // newRange.deleteContents()
+      // newRange.insertNode(document.createTextNode(text))
+
+      document.execCommand('insertText', false, text)
+    }
+  }
 }
 
 
@@ -85,26 +96,3 @@ type InputType = LiteralUnion<
   // formatXxxx 均为修改文本样式 input
   | 'formatBold'
 >
-
-
-function plaintextEvent(e: React.ClipboardEvent | React.DragEvent) {
-  const nativeEvent = e.nativeEvent
-  const dataInput = nativeEvent instanceof ClipboardEvent
-    ? nativeEvent.clipboardData
-    : nativeEvent.dataTransfer
-  if (!dataInput) return
-  const htmlOrigin = dataInput.getData('text/html')
-  const textPlain = dataInput.getData('text/plain')
-
-  if (htmlOrigin) {
-    e.preventDefault()
-
-    // FIXME: 从自己内选中内容拖放时，位置不对
-    const lastRange = window.getSelection()?.getRangeAt(0)
-    if (!lastRange) return
-    const newNode = document.createTextNode(textPlain)
-    lastRange.deleteContents()
-    lastRange.insertNode(newNode)
-    lastRange.setStartAfter(newNode)
-  }
-}
