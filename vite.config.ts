a8cb13bf67f5d3ecaab6bc9ebdd9dd87bf8ba774
N <34268416+ziloen@users.noreply.github.com>
2023-10-29 04:19:24 +0800

    chore: remove polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -63,57 +63,6 @@ export default defineConfig(({ command, mode }) => {
         ],
         dts: 'src/types/auto-imports.d.ts'
       }),
-
-      // Auto import react component?
-
-      legacy({
-      // render legacy chunks for non-modern browsers
-        renderLegacyChunks: false,
-        /** polyfills for non-modern browsers (not supports esm) */
-        // polyfills: [],
-        /** polyfills for modern browsers (supports esm) */
-        modernPolyfills: [
-        // Web APIs
-        /** structuredClone() */
-          'web.structured-clone',
-          /** URL.canParse() */
-          'web.url.can-parse',
-
-          // ES2023
-          /** Array.prototype.findLast() */
-          'es.array.find-last',
-          /** Array.prototype.findLastIndex() */
-          'es.array.find-last-index',
-          /** TypedArray.prototype.findLast() */
-          'es.typed-array.find-last',
-          /** TypedArray.prototype.findLastIndex() */
-          'es.typed-array.find-last-index',
-          /** Array.prototype.toReversed() */
-          'esnext.array.to-reversed',
-          /** Array.prototype.toSorted() */
-          'esnext.array.to-sorted',
-          /** Array.prototype.toSpliced() */
-          'esnext.array.to-spliced',
-          /** Array.prototype.with() */
-          'esnext.array.with',
-
-          // ES2022
-          /** Array.prototype.at() */
-          'es.array.at',
-          /** String.prototype.at() */
-          'es.string.at-alternative',
-          /** TypedArray.prototype.at() */
-          'es.typed-array.at',
-          /** Object.hasOwn */
-          'es.object.has-own',
-          /** AggregateError */
-          'es.aggregate-error',
-          /** AggregateError: cause */
-          'es.aggregate-error.cause',
-          /** Error: cause */
-          'es.error.cause',
-        ]
-      })
     ],
 
     css: {
