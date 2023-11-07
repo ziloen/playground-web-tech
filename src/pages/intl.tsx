139c1538703055150dc9a5a9679c0f61ef7642f8
N <34268416+ziloen@users.noreply.github.com>
2023-11-07 22:46:15 +0800

    chore: update intl

--- src/pages/intl.tsx
+++ src/pages/intl.tsx
@@ -1,5 +1,105 @@
+import { Select } from 'antd'
+import { useMotionValue } from 'framer-motion'
+
+
+const Languages = [
+  'en',
+  'es',
+  'zh-Hans',
+  'zh-Hant',
+  'fr',
+  'ja',
+  'ko'
+]
 
 
 export default function IntlPage() {
-  return null
+  const [language, setLanguage] = useState('en')
+
+  return (
+    <div className='w-full h-full flex flex-col'>
+      {/* Header */}
+      <div className='px-20px py-8px bg-white/10 flex-between items-center'>
+        <div>Intl</div>
+        <LanguageSelect language={language} onChange={setLanguage} />
+      </div>
+
+      <div className='flex-1 overflow-y-auto px-20px py-10px'>
+        <TimeNow language={language} />
+      </div>
+    </div>
+  )
+}
+
+
+function LanguageSelect({
+  language,
+  onChange
+}: {
+  language: string
+  onChange: (language: string) => void }
+) {
+  const options = useMemo(() => {
+    return Languages.map(lang => ({
+      label: (
+        <div className='flex-between items-center gap-2em'>
+          <span>{getLanguageDisplayName(language, lang)}</span>
+          <span>{getLanguageDisplayName(lang, lang)}</span>
+        </div>
+      ),
+      value: lang
+    }))
+  }, [language])
+
+  return (
+    <Select<string>
+      defaultValue={language}
+      options={options}
+      onChange={onChange}
+      popupMatchSelectWidth={false}
+      placement="bottomRight"
+      bordered={false}
+    />
+  )
+}
+
+
+function TimeNow({ language }: { language: string }) {
+  const formatter = useMemo(() => new Intl.DateTimeFormat(language, {
+    weekday: 'short',
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: false,
+    hourCycle: 'h23',
+  }), [language])
+
+  const nowStr = useMotionValue('')
+
+  useEffect(() => {
+    let animationFrameId: number
+
+    function updateNow() {
+      nowStr.set(formatter.format(new Date()))
+      animationFrameId = requestAnimationFrame(updateNow)
+    }
+
+    animationFrameId = requestAnimationFrame(updateNow)
+
+    return () => cancelAnimationFrame(animationFrameId)
+  }, [formatter])
+
+  return <motion.div>{nowStr}</motion.div>
+}
+
+
+function getLanguageDisplayName(
+  displayLanguage: string,
+  language: string,
+) {
+  const displayNames = new Intl.DisplayNames([displayLanguage], { type: 'language' })
+  return displayNames.of(language)
 }
