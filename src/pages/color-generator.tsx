7ba8be453965b3aa94b0608a35fef1983e07dec0
N <34268416+ziloen@users.noreply.github.com>
2023-11-17 17:28:55 +0800

    chore: add color space gradient comapre

--- src/pages/color-generator.tsx
+++ src/pages/color-generator.tsx
@@ -1,8 +1,12 @@
-import { Input } from 'antd'
+import { ColorPicker, Input, Slider } from 'antd'
 import Color from 'colorjs.io'
 
 export default function ColorGenerator() {
   const [inputVal, setInputVal] = useState('#fff')
+
+  useEffect(() => {
+    console.log(Color.spaces)
+  }, [])
 
   const bgColor = useMemo(() => {
     const color = new Color(inputVal)
@@ -35,6 +39,81 @@ export default function ColorGenerator() {
           '--active-bg': activeColor,
         }}
       >Test Button</button>
+
+      <ColorCompare />
+    </div>
+  )
+}
+
+
+const colorPalette = [
+  ['#00F260', '#0575E6'],
+  ['#c0c0aa', '#1cefff'],
+  ['#00c3ff', '#ffff1c'],
+  ['#f7ff00', '#db36a4'],
+  ['#fc00ff', '#00dbde'],
+  ['#03001e', '#fdeff9'],
+  ['#FC5C7D', '#6A82FB'],
+]
+
+function ColorCompare() {
+  const colorSpaces = useMemo(() => ['oklab', 'oklch', 'lch', 'hsl', 'srgb'], [])
+  const [steps, setSteps] = useState(30)
+  const [[startColor, endColor], setGradient] = useState(['#ffffff', '#000000'])
+
+  const [start, end] = useMemo(() => [new Color(startColor), new Color(endColor)], [startColor, endColor])
+
+  const gradients = useMemo(() => {
+    return colorSpaces.map(space => {
+      const range = start.range(end, { space, outputSpace: 'srgb' })
+
+      return {
+        name: Color.spaces[space].name,
+        gradient: Array
+          .from({ length: steps + 1 })
+          .map((_, i) => {
+            const color = range(i / steps)
+            return color.toString()
+          })
+          .join(',')
+      }
+    })
+  }, [start, end, steps])
+
+
+  return (
+    <div>
+      <div className='w-200px'>
+        <Slider value={steps} min={3} max={100} onChange={setSteps} />
+      </div>
+
+      <div className='flex-align gap-6px'>
+        {colorPalette.map(([start, end], i) => (
+          <div
+            key={i}
+            className='flex border cursor-pointer'
+            onClick={() => setGradient([start, end])}
+          >
+            <div
+              className='w-16px h-16px'
+              style={{ background: start }}
+            />
+            <div
+              className='w-16px h-16px'
+              style={{ background: end }}
+            />
+          </div>
+        ))}
+      </div>
+
+      <div>
+        {gradients.map(({ name, gradient }) => (
+          <div key={name} className="flex-between items-center w-[300px]">
+            <div>{name}</div>
+            <div className='w-[200px] h-[16px] rounded-4px' style={{ background: 'linear-gradient(to right, var(--stops))', '--stops': gradient }}></div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
