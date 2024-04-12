9387e487a60e2be61de4ad180d2623527dce59f8
N <34268416+ziloen@users.noreply.github.com>
2024-04-12 12:53:33 +0800

    Add exif package

--- src/pages/image-exif/index.tsx
+++ src/pages/image-exif/index.tsx
@@ -1,7 +1,33 @@
+import { load } from 'exifreader'
+
+// https://github.com/mattiasw/ExifReader
+
 export default function ImageEXIF() {
+  const [exifStr, setExifStr] = useState('')
+
+  function onChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const file = e.target.files?.[0]
+    if (!file) return
+
+    file.arrayBuffer().then(buffer => {
+      const exifData = load(buffer)
+      setExifStr(JSON.stringify(exifData, null, 2))
+    })
+  }
+
   return (
     <div>
-      ImageEXIF dajdoiwjadoawoijdi
+      <input type="file" accept="image/*" onChange={onChange} />
+
+      <p
+        style={{
+          overflowWrap: 'break-word',
+          whiteSpace: 'pre-wrap',
+          lineBreak: 'auto',
+        }}
+      >
+        {exifStr}
+      </p>
     </div>
   )
 }
