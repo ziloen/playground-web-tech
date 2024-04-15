35f5b8afa7258e96e83633df75212c769e9338bd
N <34268416+ziloen@users.noreply.github.com>
2024-04-15 15:30:13 +0800

    Update deps & Add warframe

---
+++ src/pages/warframe/index.tsx
@@ -0,0 +1,24 @@
+// syndicateMissions: The Holdfasts, Cavia
+// globalUpgrades
+// duviriCycle
+// alerts
+// Riven trading
+export default function Warframe() {
+  useEffect(() => {
+    const apiBase = 'https://api.warframestat.us'
+    const url = new URL('/pc', apiBase)
+    // url.searchParams.set('language', 'en')
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+      })
+  }, [])
+
+  return (
+    <div>
+      syndicateMissions
+    </div>
+  )
+}
