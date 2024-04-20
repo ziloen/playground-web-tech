17fb35bcb197851813a827f1e19ba860f8bc6075
N <34268416+ziloen@users.noreply.github.com>
2024-04-20 10:29:05 +0800

    Update ref links

--- src/pages/color-generator.tsx
+++ src/pages/color-generator.tsx
@@ -1,5 +1,7 @@
 //
 // https://uicolors.app
+// https://www.radix-ui.com/colors/custom
+// https://github.com/proteanstudio/contrast-tools
 
 import { ColorPicker, Input, Slider } from 'antd'
 import Color from 'colorjs.io'
