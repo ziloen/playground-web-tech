bd589210751e35129412dd6ac4280f0c3106efde
N <34268416+ziloen@users.noreply.github.com>
2024-06-07 12:42:49 +0800

    Update dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,5 +2,6 @@ import { defineConfig, preset } from "@ziloen/tailwind-config"
 
 export default defineConfig({
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  darkMode: ['variant', [`[data-theme="dark"] &`, `:host([data-theme="dark"]) &`],],
   presets: [preset]
 })
