1f9974987b738b4d4b8c778164536b1c9ea6450e
N <34268416+ziloen@users.noreply.github.com>
2024-06-18 16:24:37 +0800

    Support nested route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { isString } from 'lodash-es'
+import type { RouteObject } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import routes from '~react-pages'
 
 export default function Index() {
   const flattenedRoutes = useMemo(() => {
-    return routes
-      .map(route => route.path)
-      .filter(isString)
+    return flatRoutes(routes)
       .filter(route => !['*', '/', ':'].includes(route[0]))
       // eslint-disable-next-line @typescript-eslint/unbound-method
-      .toSorted((new Intl.Collator()).compare)
+      .toSorted((new Intl.Collator('en')).compare)
   }, [])
 
   return (
@@ -19,4 +17,19 @@ export default function Index() {
       </div>
     </div>
   )
+}
+
+function flatRoutes(
+  routes: RouteObject[],
+  parentPath: string = ''
+): string[] {
+  return routes
+    .flatMap(route => {
+      if (typeof route.path !== 'string') return []
+      const path = parentPath ? `${parentPath}/${route.path}` : route.path
+
+      return route.children
+        ? flatRoutes(route.children, path)
+        : path
+    })
 }
--- src/pages/scrollbar/index.tsx
+++ src/pages/scrollbar/index.tsx
@@ -2,9 +2,14 @@ import styles from './index.module.css'
 
 export default function Scrollbar() {
   return (
-    <div className={clsx(styles.scrollbar, 'w-200px h-400px bg-[#24292e] of-auto rounded-24px')}>
-      <div className="h-100px bg-emerald"></div>
-      <div className="h-800px"></div>
+    <div
+      className={clsx(
+        styles.scrollbar,
+        'w-[200px] h-[400px] bg-[#24292e] overflow-auto rounded-[24px]'
+      )}
+    >
+      <div className="h-[100px] bg-emerald"></div>
+      <div className="h-[800px]"></div>
     </div>
   )
 }
