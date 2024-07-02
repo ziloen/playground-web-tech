ca4b310c074a01775c4bc5b94ffaca6f70ae71c9
N <34268416+ziloen@users.noreply.github.com>
2024-07-02 16:09:05 +0800

    Add html2canvas

--- src/pages/dom-to-image.tsx
+++ src/pages/dom-to-image.tsx
@@ -1,4 +1,5 @@
 import { Button } from 'antd'
+import html2canvas from 'html2canvas'
 
 export default function DOMToImage() {
   const containerRef = useRef<HTMLDivElement>(null)
@@ -7,52 +8,24 @@ export default function DOMToImage() {
     const element = containerRef.current
     if (!element) return
 
-    const xmlns = 'http://www.w3.org/2000/svg'
-    const svg = document.createElementNS(xmlns, 'svg')
-    const foreignObject = document.createElementNS(xmlns, 'foreignObject')
+    html2canvas(element, { backgroundColor: 'black' }).then(
+      canvas => {
+        const imageUrl = canvas.toDataURL('image/png')
 
-    svg.setAttribute('width', String(element.offsetWidth))
-    svg.setAttribute('height', String(element.offsetHeight))
-
-    foreignObject.setAttribute('width', '100%')
-    foreignObject.setAttribute('height', '100%')
-    foreignObject.setAttribute('x', '0')
-    foreignObject.setAttribute('y', '0')
-    foreignObject.setAttribute('externalResourcesRequired', 'true')
-
-    svg.append(foreignObject)
-    foreignObject.append(element.cloneNode(true))
-
-    const svgString = new XMLSerializer().serializeToString(svg)
-    const svgDataBase64 = btoa(unescape(encodeURIComponent(svgString)))
-
-    const imageUrl = `data:image/svg+xml;base64,${svgDataBase64}`
-
-    const img = new Image()
-    img.src = imageUrl
-    img.onload = () => {
-      const canvas = document.createElement('canvas')
-      canvas.width = element.offsetWidth
-      canvas.height = element.offsetHeight
-
-      const context = canvas.getContext('2d')
-      if (!context) return
-
-      context.drawImage(img, 0, 0)
-
-      const canvasImageUrl = canvas.toDataURL('image/png')
-
-      const a = document.createElement('a')
-      a.href = canvasImageUrl
-      a.download = 'dom-to-image.png'
-      a.click()
-    }
+        const a = document.createElement('a')
+        a.href = imageUrl
+        a.download = 'dom-to-image.png'
+        a.click()
+      }
+    )
   }
 
   return (
     <div>
-      <div ref={containerRef} className='text-white'>
-        dom to image
+      <div className="p-2">
+        <div ref={containerRef} className="text-white border rounded-2xl border-red-200">
+          dom to image
+        </div>
       </div>
 
       <Button onClick={captureDOM}>
