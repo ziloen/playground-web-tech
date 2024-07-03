e4968e2fb83273d9524b16339cb08b19866f8c84
N <34268416+ziloen@users.noreply.github.com>
2024-07-03 12:57:49 +0800

    Change to process env

--- vite.config.ts
+++ vite.config.ts
@@ -19,9 +19,9 @@ export default defineConfig(({ command, mode }) => {
 
   let base = env.VITE_BASE_URL
 
-  if (env.GITHUB_REPOSITORY) {
-    const slashIndex = env.GITHUB_REPOSITORY.indexOf('/')
-    base = env.GITHUB_REPOSITORY.slice(slashIndex)
+  if (process.env.GITHUB_REPOSITORY) {
+    const slashIndex = process.env.GITHUB_REPOSITORY.indexOf('/')
+    base = process.env.GITHUB_REPOSITORY.slice(slashIndex)
   }
 
   return {
