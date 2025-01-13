253037c96f3b148fd920e34b5799742749f1e5cc
N <34268416+ziloen@users.noreply.github.com>
2025-01-13 18:29:12 +0800

    update deps

--- src/hooks/useGetState.ts
+++ src/hooks/useGetState.ts
@@ -1,5 +1,5 @@
-export function useGetState<T>(value: T | (() => T)) {
-  const [state, _setState] = useState<T>(value)
+export function useGetState<T>(initialState: T | (() => T)) {
+  const [state, _setState] = useState<T>(initialState)
   const stateRef = useRef<T>(state)
 
   const getState = useCallback(() => stateRef.current, [])
