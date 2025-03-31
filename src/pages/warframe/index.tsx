fb7e3a12c37b76da4c8b8135423d34ccff50a5d7
N <34268416+ziloen@users.noreply.github.com>
2025-03-31 13:34:06 +0800

    update warframe page

--- src/pages/warframe/index.tsx
+++ src/pages/warframe/index.tsx
@@ -6,6 +6,7 @@
 // Ticker
 import { useQuery } from '@tanstack/react-query'
 import { asNonNullable } from '@wai-ri/core'
+import { Temporal } from 'temporal-polyfill'
 import { getWorldStateApi } from '~/api/warframe'
 
 export default function Warframe() {
@@ -27,27 +28,67 @@ export default function Warframe() {
   console.log(data)
 
   return (
-    <div>
-      <div>
-        {data.news.map((news) => {
-          return (
-            <div key={news.id}>
-              <div>
-                {news.message}
-              </div>
+    <div
+      className="grid gap-1 w-fit max-w-[500px]"
+      style={{
+        gridTemplateColumns: 'max-content 1fr',
+      }}
+    >
+      {data.news.toReversed().map((news, index) => {
+        console.log(Temporal)
+        const date = Temporal.Instant.from(news.date).toZonedDateTimeISO('UTC')
+        const now = Temporal.Now.zonedDateTimeISO('UTC')
 
+        const diff = date.until(now, {
+          largestUnit: 'years',
+          smallestUnit: 'days',
+        })
+
+        const rtf = new Intl.RelativeTimeFormat('en', {
+          style: 'narrow',
+          numeric: 'always',
+        })
+
+        return (
+          <a
+            href={news.link}
+            key={news.id}
+            target="_blank"
+            rel="noreferrer"
+            className="grid relative border border-solid bg-dark-gray-900 border-dark-gray-300 rounded-sm p-2 gap-2 no-underline text-light-gray-200 visited:text-light-gray-900 grid-cols-subgrid col-span-full"
+          >
+            {index === 0 && (
               <img
-                className="w-[36px] aspect-video"
+                className="aspect-video col-span-full w-full"
+                style={{
+                  contain: 'size',
+                  containIntrinsicSize: '0 auto',
+                }}
                 src={news.imageLink}
                 alt="News image"
                 decoding="async"
                 fetchPriority="low"
                 loading="lazy"
+                crossOrigin="anonymous"
               />
+            )}
+
+            <div className="grid gap-3 grid-cols-subgrid col-span-2">
+              <span>
+                {diff.years > 0
+                  ? rtf.format(diff.years, 'year')
+                  : diff.months > 0
+                  ? rtf.format(diff.months, 'month')
+                  : rtf.format(diff.days, 'day')}
+              </span>
+
+              <span>
+                {news.message}
+              </span>
             </div>
-          )
-        })}
-      </div>
+          </a>
+        )
+      })}
     </div>
   )
 }
