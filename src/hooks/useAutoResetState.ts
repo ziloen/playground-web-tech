1b3bb1d1489efaf14ad268ccf29bc10f1eaadd33
N <34268416+ziloen@users.noreply.github.com>
2025-06-27 19:05:16 +0800

    update deps

--- src/hooks/useAutoResetState.ts
+++ src/hooks/useAutoResetState.ts
@@ -5,7 +5,7 @@ import { useMemoizedFn } from './useMemoizedFn'
 /**
  * 自动重置状态
  * @param defautValue 初始值 & 重置状态值
- * @param afterMs 自动重置时间，单位毫秒 ms，默认 1000ms
+ * @param resetMilliseconds 自动重置时间，单位毫秒 ms，默认 1000ms
  * @example
  * ```tsx
  * const [copied, setCopied] = useAutoResetState(false, 1_000)
@@ -24,7 +24,7 @@ import { useMemoizedFn } from './useMemoizedFn'
  */
 export function useAutoResetState<T>(
   defautValue: T,
-  afterMs = 1_000,
+  resetMilliseconds = 1_000,
 ): [T, Dispatch<SetStateAction<T>>] {
   const [state, setState] = useState<T>(defautValue)
   const timer = useRef<number | undefined>(undefined)
@@ -32,7 +32,7 @@ export function useAutoResetState<T>(
   const set: typeof setState = useMemoizedFn((value) => {
     setState(value)
     clearTimeout(timer.current)
-    timer.current = setTimeout(() => setState(defautValue), afterMs)
+    timer.current = setTimeout(() => setState(defautValue), resetMilliseconds)
   })
 
   useEffect(() => () => clearTimeout(timer.current), [])
