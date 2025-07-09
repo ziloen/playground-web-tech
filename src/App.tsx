0e3ac77b2db5bc4dfb8221ac2c8671da57f79b8e
N <34268416+ziloen@users.noreply.github.com>
2025-07-09 16:03:08 +0800

    use react-router Data mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,17 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { Suspense } from 'react'
-import { BrowserRouter } from 'react-router'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 import routes from '~react-pages'
 
-function Routes() {
-  return useRoutes(routes)
-}
-
-// Create a client
 const queryClient = new QueryClient()
+
+const router = createBrowserRouter(routes, {
+  basename: import.meta.env.BASE_URL,
+})
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
-        <Suspense>
-          <Routes />
-        </Suspense>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   )
 }
