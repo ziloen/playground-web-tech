bc4fc204df78907fc6e2c34eb10f2841e5d80db7
N <34268416+ziloen@users.noreply.github.com>
2025-08-28 11:47:51 +0800

    fix scrollbar property inheritance issue

--- src/pages/scrollbar/_index.tsx
+++ src/pages/scrollbar/_index.tsx
@@ -7,6 +7,10 @@ export default function Scrollbar() {
         styles.scrollbar,
         'size-[400px] bg-[#24292e] overflow-auto rounded-[24px]',
       )}
+      style={{
+        '--scrollbar-margin-block': '24px',
+        '--scrollbar-margin-inline': '24px',
+      }}
     >
       <div className="size-[100px] bg-green-800"></div>
       <div className="size-[800px]"></div>
