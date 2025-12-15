7094c8184c1bd16e959956bde970ab3db32c51b2
N <34268416+ziloen@users.noreply.github.com>
2025-12-15 20:06:09 +0800

    chore: update deps

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,66 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { isNotNil } from 'es-toolkit'
+import type { ComponentType } from 'react'
 import type { LoaderFunction, RouteObject } from 'react-router'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 
-const queryClient = new QueryClient({
+export const queryClient = new QueryClient({
   defaultOptions: {
-    queries: {
-      retry: false,
-    },
-    mutations: {
-      retry: false,
-    },
+    queries: { retry: false },
+    mutations: { retry: false },
   },
 })
 
-function DefaultHydrateFallback() {
+function HydrateFallback() {
   return null
 }
 
-export const routes = Object
-  .entries(
-    import.meta.glob<{
-      default: React.ComponentType
-      loader?: LoaderFunction
-      HydrateFallback?: React.ComponentType
-      ErrorBoundary?: React.ComponentType
-    }>('./pages/**/*.tsx'),
-  )
-  .map<RouteObject | null>(([path, request]) => {
-    const filePath = path.slice(8, -4)
+export const routes = Object.entries(
+  import.meta.glob<{
+    default: ComponentType
+    loader?: LoaderFunction
+    handle?: unknown
+    HydrateFallback?: ComponentType
+    ErrorBoundary?: ComponentType
+  }>('./**/*.tsx', { base: './pages' }),
+).map<RouteObject>(([path, request]) => {
+  // `./concerts.$id.tsx` -> `concerts.$id`
+  const filePath = path.slice(2, -4)
 
-    if (!filePath) {
-      return null
-    }
+  const index = filePath.endsWith('_index')
 
-    const index = filePath.endsWith('_index')
+  // https://reactrouter.com/how-to/file-route-conventions#file-route-conventions
+  const normalizedPath = filePath
+    // Index Route: `_index.tsx` -> `/`
+    .replaceAll('_index', '')
+    // Catch-all Route: `$.tsx` -> `*`
+    .replaceAll(/\$$/g, '*')
+    // Optional Segments: `($lang).$id.tsx` -> `:lang?/:id`, `item.(edit).tsx` -> `item/edit?`
+    .replaceAll(/\(([^).]+)\)/g, '$1?')
+    // Dynamic Segments: `item.$id.tsx` -> `item/:id`
+    .replaceAll('$', ':')
+    // Nested Route: `concerts.trending.tsx` -> `concerts/trending`
+    .replaceAll('.', '/')
 
-    const normalizedPath = filePath
-      .replaceAll('_index', '')
-      .replaceAll(/\$$/g, '*')
-      .replaceAll('$', ':')
-      .replaceAll('.', '/')
+  return {
+    index: index,
+    path: normalizedPath,
+    HydrateFallback,
+    lazy: async () => {
+      const route = await request()
 
-    return {
-      index: index,
-      path: normalizedPath,
-      HydrateFallback: DefaultHydrateFallback,
-      lazy: async () => {
-        const value = await request()
-        return {
-          Component: value.default,
-          loader: value.loader,
-          HydrateFallback: value.HydrateFallback ?? null,
-          ErrorBoundary: value.ErrorBoundary ?? null,
-        }
-      },
-    }
-  })
-  .filter(isNotNil)
+      return {
+        loader: route.loader,
+        handle: route.handle,
+        Component: route.default,
+        // FIXME: HydrateFallback is not working in lazy routes
+        HydrateFallback: route.HydrateFallback ?? null,
+        ErrorBoundary: route.ErrorBoundary ?? null,
+      }
+    },
+  }
+})
 
-const router = createBrowserRouter(routes, {
+export const router = createBrowserRouter(routes, {
   basename: import.meta.env.BASE_URL,
 })
 
