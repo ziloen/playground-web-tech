b77fa975cf90f0363ffabf258a388ba3e1a962ea
N <34268416+ziloen@users.noreply.github.com>
2026-07-07 22:50:20 +0800

    fix: build

--- src/pages/paint.tsx
+++ src/pages/paint.tsx
@@ -1,16 +1,17 @@
-import { trackPointerMove } from '~/hooks'
+import type { RefCallback } from 'react'
+import { trackPointerMove, useMemoizedFn } from '~/hooks'
 
 export default function PaintPage() {
   return (
     <div className="relative grid size-full grid-cols-[minmax(0,max-content)] grid-rows-[minmax(0,max-content)] place-content-center">
       <img
         src="https://dummyjson.com/image/960x540/333/fff?text=960x540"
-        alt="500x500"
-        className="col-[1/2] row-[1/2] max-h-full max-w-full"
+        alt="960x540"
+        className="max-h-full max-w-full area-[1/1/2/2]"
       />
 
       <canvas
-        className="absolute inset-0 col-[1/2] row-[1/2] size-full bg-green/10"
+        className="absolute inset-0 size-full bg-linear-to-r from-green/10 to-red/10 area-[1/1/2/2]"
         onPointerDown={(e) => {
           trackPointerMove(e, {
             captureOn: 'pointerdown',
